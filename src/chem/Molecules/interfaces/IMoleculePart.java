bd981de7867b06b995add8420e46d614f6af2071
dmitriy.selischev <zibertscrem@gmail.com>
2016-07-09 01:56:59 +0300

    Implemented molecule classes

---
+++ src/chem/Molecules/interfaces/IMoleculePart.java
@@ -0,0 +1,15 @@
+package chem.Molecules.interfaces;
+
+import chem.interfaces.IElement;
+
+/**
+ * Created by Дмитрий on 09.07.2016.
+ */
+public interface IMoleculePart
+{
+    IMoleculePart[] getCompositePart();
+    IElement getSinglePart();
+    boolean isComposite();
+    boolean isSingle();
+    double getMass();
+}

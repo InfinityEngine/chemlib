7d4153a42982c676b8f6cb3c23eae0a719fa473a
InfinityEngine <oleg.chigirin@gmail.com>
2016-07-09 01:10:28 +0300

    Starded implementing elements interface

---
+++ src/chem/interfaces/IElement.java
@@ -0,0 +1,11 @@
+package chem.interfaces;
+
+/**
+ * Created by TwilightSparkle on 09.07.2016.
+ */
+public interface IElement {
+
+    double getRelativeMass();
+    String getSymbol();
+    int getNuclearCharge();
+}
